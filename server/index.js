b9e533413ba06e3e7b33c7b7b5affaebe0c9faa4
Diego-Paris <45906185+Diego-Paris@users.noreply.github.com>
2020-04-19 20:26:10 -0400

    Added .gitignore

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
+// when you get a 'get' request on the / route, run this function
+app.get("/", (request, response) => {
+  response.json({
+    message: "Meow!!!",
+  });
+});
+
 app.listen(5000, () => {
-    console.log('Listening on http://localhost:5000');
-})
+  console.log("Listening on http://localhost:5000");
+});
