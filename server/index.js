17057c522508f3917079c67c03cc08a6b21f9194
Diego-Paris <45906185+Diego-Paris@users.noreply.github.com>
2020-04-21 10:54:30 -0400

    Added profanity filter and rate limiter

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const monk = require("monk");
+const Filter = require("bad-words");
+const rateLimit = require("express-rate-limit");
 
 const app = express();
 
-const db = monk("localhost/meower");
+const db = monk(process.env.MONGO_URI || "localhost/meower");
 const mews = db.get("mews");
+const filter = new Filter();
 
 app.use(cors());
 app.use(express.json());
+
 
 // when you get a 'get' request on the / route, run this function
 app.get("/", (req, res) => {
@@ -34,12 +38,17 @@ function isValidMew(mew) {
   );
 }
 
+app.use(rateLimit({
+  windowMs: 30 * 1000,   // determines the milliseconds 
+  max: 1          // limit each IP to specified number of requests per windowMs
+}));
+
 app.post("/mews", (req, res) => {
   if (isValidMew(req.body)) {
     //insert DB
     const mew = {
-      name: req.body.name.toString(),
-      content: req.body.content.toString(),
+      name: filter.clean(req.body.name.toString()),
+      content: filter.clean(req.body.content.toString()),
       created: new Date()
     };
     console.log(mew);
